de80e4b2b40e016739556c85dea004f9f06eadf4
Stanislav Savulchik <s.savulchik@gmail.com>
2014-01-04 19:49:46 +0700

    Добавлен bootstrap.sh файл

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DOTFILES_DIR=$(cd $(dirname $0); pwd -P)
+
+ln -sfv $DOTFILES_DIR/gitconfig $HOME/.gitconfig
+ln -sfvT $DOTFILES_DIR/vim $HOME/.vim
+ln -sfv $DOTFILES_DIR/vimrc $HOME/.vimrc
+ln -sfvT $DOTFILES_DIR/zsh $HOME/.zsh
+ln -sfv $DOTFILES_DIR/zshrc $HOME/.zshrc
