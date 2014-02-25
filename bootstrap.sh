d407adaa750e94df77f842748585eef1f8126027
Stanislav Savulchik <s.savulchik@gmail.com>
2014-02-25 22:53:18 +0700

    Добавлен gemrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,5 @@ ln -sfvT $DOTFILES_DIR/vim $HOME/.vim
 ln -sfv $DOTFILES_DIR/vimrc $HOME/.vimrc
 ln -sfvT $DOTFILES_DIR/zsh $HOME/.zsh
 ln -sfv $DOTFILES_DIR/zshrc $HOME/.zshrc
+ln -sfv $DOTFILES_DIR/gemrc $HOME/.gemrc
+
