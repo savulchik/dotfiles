0291ac6b2bf8f6d85c744619914b069de37dbe4e
Stanislav Savulchik <s.savulchik@gmail.com>
2014-10-05 17:12:02 +0700

    Адаптирован bootstrap.sh для mac

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 DOTFILES_DIR=$(cd $(dirname $0); pwd -P)
 
 ln -sfv $DOTFILES_DIR/gitconfig $HOME/.gitconfig
-ln -sfvT $DOTFILES_DIR/vim $HOME/.vim
+ln -sfvn $DOTFILES_DIR/vim $HOME/.vim
 ln -sfv $DOTFILES_DIR/vimrc $HOME/.vimrc
-ln -sfvT $DOTFILES_DIR/zsh $HOME/.zsh
+ln -sfvn $DOTFILES_DIR/zsh $HOME/.zsh
 ln -sfv $DOTFILES_DIR/zshrc $HOME/.zshrc
 ln -sfv $DOTFILES_DIR/gemrc $HOME/.gemrc
 
